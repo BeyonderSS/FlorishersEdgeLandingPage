f1e52c67fab535adfebd8d94f5d328c0f67a4c89
BeyonderSS <ssbeyonder@gmail.com>
2023-05-05 14:44:11 +0530

    Headding + Favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Head>
+      <title>Flourishers Edge</title>
+      <meta name="description" content="Flourishers Edge website description." />
+      <link rel="icon" href="/favicon.png" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" />
+      <Component {...pageProps} />
+    </Head>
+  )
 }
