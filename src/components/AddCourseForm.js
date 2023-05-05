36a9972103a972128f06551f3526dea014de53ea
BeyonderSS <ssbeyonder@gmail.com>
2023-05-05 14:18:46 +0530

    Course Cards

--- src/components/AddCourseForm.js
+++ src/components/AddCourseForm.js
@@ -3,46 +3,37 @@ import { collection, addDoc, getDocs, where, query } from "firebase/firestore";
 import { firestore } from "../utils/initFirebase";
 
 const AddCourseForm = () => {
-  const [id, setId] = useState("");
-  const [heading, setHeading] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!id || !heading || !description || !price) {
-      setErrorMessage("Please fill in all fields");
-      return;
-    }
     setLoading(true);
     try {
       const coursesCollection = collection(firestore, "Courses");
-      const querySnapshot = await getDocs(
-        query(coursesCollection, where("id", "==", id))
-      );
-      if (!querySnapshot.empty) {
-        setErrorMessage("ID already exists");
+      const fileReader = new FileReader();
+      fileReader.readAsText(selectedFile);
+      fileReader.onload = async () => {
+        const data = JSON.parse(fileReader.result);
+        for (const course of data.courses) {
+          await addDoc(coursesCollection, {
+            mainHeading: course.mainHeading,
+            subHeading: course.subHeading,
+            price: course.price,
+          });
+        }
+        setErrorMessage("");
         setLoading(false);
-        return;
-      }
-      await addDoc(coursesCollection, {
-        id,
-        heading,
-        description,
-        price,
-      });
-      setId("");
-      setHeading("");
-      setDescription("");
-      setPrice("");
-      setErrorMessage("");
-      setLoading(false);
-      alert("Course added successfully");
+        alert("Courses added successfully");
+      };
     } catch (error) {
       console.error(error);
-      setErrorMessage("Error adding course");
+      setErrorMessage("Error adding courses");
       setLoading(false);
     }
   };
@@ -53,69 +44,28 @@ const AddCourseForm = () => {
         onSubmit={handleSubmit}
         className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
       >
-        <h2 className="text-2xl font-bold mb-4">Add Course</h2>
+        <h2 className="text-2xl font-bold mb-4">Add Courses</h2>
         {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
         <div className="mb-4">
-          <label htmlFor="id" className="block text-gray-700 font-bold mb-2">
-            ID
+          <label className="block text-gray-700 font-bold mb-2" htmlFor="file">
+            Upload JSON file:
           </label>
           <input
-            type="text"
-            name="id"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="heading"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Heading
-          </label>
-          <input
-            type="text"
-            name="heading"
-            value={heading}
-            onChange={(e) => setHeading(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="description"
-            className="block text-gray-700 font-bold mb-2"
-          >
-            Description
-          </label>
-          <input
-            type="text"
-            name="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-        <div className="mb-6">
-          <label htmlFor="price" className="block text-gray-700 font-bold mb-2">
-            Price
-          </label>
-          <input
-            type="text"
-            name="price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            type="file"
+            id="file"
+            onChange={handleFileChange}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           />
         </div>
         <div className="flex items-center justify-between">
           <button
+            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ${
+              loading ? "opacity-50 cursor-wait" : ""
+            }`}
             type="submit"
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            disabled={loading}
+            disabled={loading || !selectedFile}
           >
-            {loading ? "Adding Course..." : "Add Course"}
+            {loading ? "Adding..." : "Add Courses"}
           </button>
         </div>
       </form>
--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -15,44 +15,136 @@ const Pricing = () => {
     fetchCourses();
   }, []);
 
+  // Filter courses based on their subHeading value
+  const schoolCourses = courses.filter(
+    (course) => course.subHeading === "School"
+  );
+  const upscalingCourses = courses.filter(
+    (course) => course.subHeading === "Upscaling"
+  );
+  const collegeCourses = courses.filter(
+    (course) => course.subHeading === "College"
+  );
+  console.log(courses);
+
   return (
     <div>
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-white ">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
           <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              Designed for aspiring students like you, to help you thrive.
+            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 ">
+              Courses designed for aspiring students like you, to help you
+              thrive.
             </h2>
-            <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
+            <p className="mb-5 font-light text-gray-500 sm:text-xl ">
               Welcome to Flourishers Edge - where aspiring students harness the
               power of technology, innovation, and capital to create value and
               drive economic growth.
             </p>
           </div>
-          <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
-            {/* <!-- Pricing Cards --> */}
-            {courses.map((course) => (
-              <div
-                key={course.id}
-                className="flex flex-col p-6 mx-auto max-w-lg md:w-96 w-72 h-auto text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white"
-              >
-                <h3 className="mb-4 text-2xl font-semibold">
-                  {course.heading}
+          <div className="flex flex-col justify-center items-center">
+            {/* <!-- School Courses --> */}
+            {schoolCourses.length > 0 && (
+              <div>
+                <h3 className="text-4xl  flex py-5 font-semibold justify-center items-center ">
+                  {" "}
+                  <span className=" text-purple-800">S</span>chool
                 </h3>
-                <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                  {course.description}
-                </p>
-                <div className="flex justify-center items-baseline my-8">
-                  <span className="mr-2 text-5xl font-extrabold">
-                    ₹{course.price}
-                  </span>
-                </div>
+                <hr class="h-px mb-4 bg-gray-300 border-0 "></hr>
+                <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+                  {schoolCourses.map((course) => (
+                    <div className="">
+                      <div
+                        key={course.mainHeading}
+                        className="flex flex-col p-6 mx-auto max-w-lg md:w-96 w-72 h-auto text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow  xl:p-8 "
+                      >
+                        <h3 className="mb-4 text-2xl font-semibold">
+                          {course.mainHeading}
+                        </h3>
+                        <p className="font-light text-gray-500 sm:text-lg ">
+                          {course.subHeading}
+                        </p>
+                        <div className="flex justify-center items-baseline my-8">
+                          <span className="mr-2 text-5xl font-extrabold">
+                            ₹{course.price}
+                          </span>
+                        </div>
 
-                <button className="text-white cursor-pointer bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-purple-900">
-                  Get started
-                </button>
+                        <button className="text-white cursor-pointer bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+                          Contact Us
+                        </button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
               </div>
-            ))}
+            )}
+
+            {/* <!-- Upscaling Courses --> */}
+            {upscalingCourses.length > 0 && (
+              <div>
+                <h3 className="text-4xl flex py-10 font-semibold justify-center items-center">
+                  Up <span className="text-purple-800">scaling </span>
+                </h3>
+                <hr class="h-px mb-4 bg-gray-300 border-0 "></hr>
+                <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+                  {upscalingCourses.map((course) => (
+                    <div
+                      key={course.mainHeading}
+                      className="flex flex-col p-6 mx-auto max-w-lg md:w-96 w-72 h-auto text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow  xl:p-8 "
+                    >
+                      <h3 className="mb-4 text-2xl font-semibold">
+                        {course.mainHeading}
+                      </h3>
+                      <p className="font-light text-gray-500 sm:text-lg ">
+                        {course.subHeading}
+                      </p>
+                      <div className="flex justify-center items-baseline my-8">
+                        <span className="mr-2 text-5xl font-extrabold">
+                          ₹{course.price}
+                        </span>
+                      </div>
+                      <button className="text-white cursor-pointer bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+                        Contact Us
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+
+            {/* <!-- College Courses --> */}
+            {collegeCourses.length > 0 && (
+              <div>
+                <h3 className="text-4xl flex py-10 font-semibold justify-center items-center">
+                  Col <span className="text-purple-800"> lege</span>
+                </h3>
+                <hr class="h-px mb-4 bg-gray-300 border-0 "></hr>
+                <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
+                  {collegeCourses.map((course) => (
+                    <div
+                      key={course.mainHeading}
+                      className="flex flex-col p-6 mx-auto max-w-lg md:w-96 w-72 h-auto text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow  xl:p-8 "
+                    >
+                      <h3 className="mb-4 text-2xl font-semibold">
+                        {course.mainHeading}
+                      </h3>
+                      <p className="font-light text-gray-500 sm:text-lg ">
+                        {course.subHeading}
+                      </p>
+                      <div className="flex justify-center items-baseline my-8">
+                        <span className="mr-2 text-5xl font-extrabold">
+                          ₹{course.price}
+                        </span>
+                      </div>
+                      <button className="text-white cursor-pointer bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center ">
+                        Contact Us
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </section>
