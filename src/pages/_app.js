3690b92c42e8802906d6db41310bf012847c3ce7
BeyonderSS <ssbeyonder@gmail.com>
2023-05-05 14:47:57 +0530

    deployent fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,5 @@
 import '@/styles/globals.css'
-import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return (
-    <Head>
-      <title>Flourishers Edge</title>
-      <meta name="description" content="Flourishers Edge website description." />
-      <link rel="icon" href="/favicon.png" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" />
-      <Component {...pageProps} />
-    </Head>
-  )
+  return (<Component {...pageProps} />)
 }
