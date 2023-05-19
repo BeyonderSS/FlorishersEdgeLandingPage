1f70765b112816cd5c81cb90e4f1da3d8654ed49
BeyonderSS <ssbeyonder@gmail.com>
2023-05-20 01:56:42 +0530

    Final conntact us

--- src/pages/Support/ContactUs.js
+++ src/pages/Support/ContactUs.js
@@ -1,81 +1,141 @@
+import React from "react";
 import { useForm } from "react-hook-form";
-import { BiPhoneIncoming } from "react-icons/bi";
-import { BsFillEnvelopeFill, BsPinMapFill } from "react-icons/bs";
+import { BsFillEnvelopeFill, BsFillPinMapFill } from "react-icons/bs";
 
-function ContactUs({ pageInfo }) {
+const ContactUs = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (formData) => {
-    window.location.href = `mailto:flourishers.edge2023@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name},${formData.message} (${formData.email})`;
+    console.log(formData);
+    window.location.href = `mailto:flourishers.edge2023@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name} & my Phone No. is ${formData.phoneNo},${formData.message} (${formData.email})`;
   };
 
   return (
-    <div className=" bg-purple-100">
+    <>
+      <div className="bg-purple-200">
+        <section className="min-h-screen">
+          <div className=" flex"></div>
 
-    <div className="h-screen relative flex flex-col text-center md:text-left md:flex-row max-w-3xl justify-evenly mx-auto items-center md:w-full w-80 ">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
-        Contact
-      </h3>
-      <div className="flex flex-col space-y-10 mt-40 mb-5">
-        <h4 className="md:text-4xl text-lg font-semibold text-center">
-          We Have Got Just What You Need.{" "}
-          <span className="decoration-[#985DEA]/50 underline">Lets Talk.</span>
-        </h4>
-        <div className="space-y-5">
-          <div className="flex items-center space-x-1 justify-center">
-            <BiPhoneIncoming className="h-7 w-7 animate-pulse text-[#985DEA]" />
-            <p className="text-l">+917869088589</p>
+          <div className="container mx-auto px-6 md:px-12 xl:px-32">
+            <div className="text-center text-gray-900">
+              <div
+                className="block rounded-lg shadow-lg px-6 py-12 md:py-16 md:px-12"
+                style={{
+                  marginTop: 180,
+                  backgroundColor: "hsla(0, 0%, 100%, 0.8)",
+                  backdropFilter: "saturate(200%) blur(25px)",
+                }}
+              >
+                <section className="mb-12 text-gray-800">
+                  <div className="flex justify-center">
+                    <div className="text-center lg:max-w-3xl md:max-w-xl">
+                      <h2 className="text-3xl font-bold mb-12 px-6">
+                        Contact us
+                      </h2>
+                    </div>
+                  </div>
+                  <div className="flex flex-wrap">
+                    <div className="grow-0 shrink-0 basis-auto mb-8 lg:mb-0 w-full lg:w-5/12 px-3 lg:px-6">
+                      <form onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group mb-6">
+                          <input
+                            {...register("name")}
+                            type="text"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-purple-600 focus:outline-none"
+                            id="exampleInput7"
+                            placeholder="Name"
+                          />
+                        </div>
+                        <div className="form-group mb-6">
+                          <input
+                            {...register("email")}
+                            type="email"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-purple-600 focus:outline-none"
+                            id="exampleInput8"
+                            placeholder="Email address"
+                          />
+                        </div>
+                        <div className="form-group mb-6">
+                          <input
+                            {...register("subject")}
+                            type="text"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-purple-600 focus:outline-none"
+                            id="exampleInput8"
+                            placeholder="Subject"
+                          />
+                        </div>
+                        <div className="form-group mb-6">
+                          <input
+                            {...register("phoneNo")}
+                            type="text"
+                            className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-purple-600 focus:outline-none"
+                            id="exampleInput8"
+                            placeholder="Phone No"
+                          />
+                        </div>
+                        <div className="form-group mb-6">
+                          <textarea
+                            {...register("message")}
+                            className=" form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-purple-600 focus:outline-none"
+                            id="exampleFormControlTextarea13"
+                            rows={3}
+                            placeholder="Message"
+                            defaultValue={""}
+                          />
+                        </div>
+
+                        <button
+                          type="submit"
+                          className="w-full px-6 py-2.5 bg-purple-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out"
+                        >
+                          Send
+                        </button>
+                      </form>
+                    </div>
+                    <div className="grow-0 shrink-0 basis-auto w-full lg:w-7/12">
+                      <div className="flex flex-wrap">
+                        <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                          <div className="flex items-start">
+                            <div className="shrink-0">
+                              <div className="p-4 bg-purple-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                                <BsFillPinMapFill className="text-4xl text-white"/>
+                              </div>
+                            </div>
+                            <div className="grow ml-6">
+                              <p className="font-bold mb-1">Addrress</p>
+                              <p className="text-gray-500">
+                                flourishersedge near 11 miles hausangabad road
+                                Bhopal 462046
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="mb-12 grow-0 shrink-0 basis-auto w-full lg:w-6/12 px-3 lg:px-6">
+                          <div className="flex align-start">
+                            <div className="shrink-0">
+                              <div className="p-4 bg-purple-600 rounded-md shadow-md w-14 h-14 flex items-center justify-center">
+                                <BsFillEnvelopeFill className="text-4xl text-white"/>
+                              </div>
+                            </div>
+                            <div className="grow ml-6">
+                              <p className="font-bold mb-1">Email</p>
+                              <p className="text-gray-500">
+                                flourishers.edge2023@gmail.com
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+              </div>
+            </div>
           </div>
-          <div className="flex items-center space-x-1 justify-center">
-            <BsFillEnvelopeFill className="h-7 w-7 animate-pulse text-[#985DEA]" />
-            <p className="text-l">Flourishers.edge2023@gmail.com</p>
-          </div>
-          <div className="hidden md:flex items-center space-x-1 justify-center">
-            <BsPinMapFill className="h-7 w-7 animate-pulse text-[#985DEA]" />
-            <p className="text-l ">
-              flourishersedge near 11 miles hausangabad road Bhopal 462046
-            </p>
-          </div>
-        </div>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-col space-y-2 md:w-auto w-96 mx-auto "
-        >
-          <div className=" md:flex flex-row md:space-x-2 md:space-y-0 space-y-2 ">
-            <input
-              {...register("name")}
-              placeholder="Name"
-              className="outline-none w-full bg-slate-400/10 rounded-sm border-b px-6 py-4 border-[#242424] text-gray-500 placeholder-gray-500 transition-all focus:border-[#985DEA]/40 focus:text-[#985DEA]/40 hover:border-[#985DEA]/40"
-              type="text"
-            />
-            <input
-              {...register("email")}
-              placeholder="Email"
-              className="outline-none w-full bg-slate-400/10 rounded-sm border-b px-6 py-4 border-[#242424] text-gray-500 placeholder-gray-500 transition-all focus:border-[#985DEA]/40 focus:text-[#985DEA]/40 hover:border-[#985DEA]/40"
-              type="email"
-            />
-          </div>
-          <input
-            {...register("subject")}
-            placeholder="Subject"
-            className="outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-[#242424] text-gray-500 placeholder-gray-500 transition-all focus:border-[#985DEA]/40 focus:text-[#985DEA]/40 hover:border-[#985DEA]/40"
-            type="text"
-          />
-          <textarea
-            {...register("message")}
-            placeholder="Message"
-            className="outline-none bg-slate-400/10 rounded-sm border-b px-6 py-4 border-[#242424] text-gray-500 placeholder-gray-500 transition-all focus:border-[#985DEA]/40 focus:text-[#985DEA]/40 hover:border-[#985DEA]/40"
-          />
-          <button
-            type="submit"
-            className="bg-[#985DEA] py-5 px-10 rounded-md text-black font-bold text-lg"
-          >
-            Submit
-          </button>
-        </form>
+        </section>
       </div>
-    </div>
-    </div>
+    </>
   );
-}
+};
 
 export default ContactUs;
