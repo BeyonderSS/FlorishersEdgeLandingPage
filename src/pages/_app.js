1956a2e9f27c1fddb946e0d914186ddd1d8b082e
BeyonderSS <ssbeyonder@gmail.com>
2023-06-15 18:24:49 +0530

    Vercel Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import "@/styles/globals.css";
 import { IBM_Plex_Sans } from "next/font/google";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 const Ibm_Plex_Sans = IBM_Plex_Sans({
   weight: "400",
@@ -12,12 +13,13 @@ export default function App({ Component, pageProps }) {
   return (
     <main className={Ibm_Plex_Sans.className}>
       <Head>
-
-      <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="shortcut icon" href="/favicon.png" />
         <title>Flourishers Edge</title>
       </Head>
       <Header />
       <Component {...pageProps} />
+      <Analytics />
+
       <Footer />
     </main>
   );
